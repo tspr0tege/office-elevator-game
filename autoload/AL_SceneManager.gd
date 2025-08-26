4ceffc59cab35c0c3de558e63fea3e1368df4213
Squall Leonhart <squallpl1983@gmail.com>
2025-08-26 02:44:46 -0500

    Creating first scenes
    -Created autoload SceneManager
    
    -Created Office_1 as a test for SceneManager and experimenting with the
    tileset by recreating the example image that came with the sprite set.

---
+++ autoload/AL_SceneManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+const START_MENU = preload("res://scenes/start-menu.tscn")
+const OFFICE_1 = preload("res://scenes/office_1.tscn")
+
+func goto_start_menu() -> void:
+	get_tree().change_scene_to_packed(START_MENU)
+
+func goto_office_1() -> void:
+	get_tree().change_scene_to_packed(OFFICE_1)
---
+++ scenes/scenes_start-menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_start_game_pressed() -> void:
+	SceneManager.goto_office_1()
